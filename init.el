3a8c8709ba75679b1dd40bbece76d25ad80193e3
DenverEllis <ellis.denver@yahoo.com>
2019-04-05 15:36:27 -0500

    added stuff

--- init.el
+++ init.el
@@ -42,7 +42,10 @@
   (package-install 'use-package))
 
 ;; === Face Customization ==
-
+(use-package zenburn-theme
+  :ensure t
+  :init
+  (load-theme 'zenburn t))
 ;; === Interface ===
 
 ;; === Document Editing ===
@@ -52,3 +55,18 @@
 ;; === Misc. ===
 
 ;;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme use-package exec-path-from-shell diminish))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
